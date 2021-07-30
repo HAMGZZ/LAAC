93a24162360c51f1154f1a7a0cf28a6e306186b5
Lewis Hamilton <32315974+HAMGZZ@users.noreply.github.com>
2021-07-31 08:52:35 +1000

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  LAAC - Large Antenna Array Controller
+ *  Control software for DeathBox
+ *  Lewis Hamilton 2021 - 3rd Attempt August 2021
+ *  
+ */ 
+
+#include <Arduino.h>
+
+
+int main()
+{
+
+
+  while(true)
+  {
+    
+  }
+  return 0;
+}
